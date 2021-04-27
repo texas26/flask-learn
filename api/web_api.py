79fa7e19121a2860909c5cfbc4b3663cc12479ce
Tejas Pai <meettejaspai@gmail.com>
2021-04-26 22:23:50 -0700

    second commit with fixes

--- api/web_api.py
+++ api/web_api.py
@@ -1,27 +1,39 @@
 import flask
 from flask_json import FlaskJSON, json_response, as_json
 import sqlite3
+import validators
 
 app = flask.Flask(__name__)
 FlaskJSON(app)
 
-@app.route('/urlinfo/1/<hostname_port>/<path_and_query>', methods=['GET'])
-def check_url(hostname_port, path_and_query):
-    url_unsafe = False
-    url_infected = False
-    query_infected = False
+@app.route('/urlinfo/1/<hostname_port>/')
+@app.route('/urlinfo/1/<hostname_port>/<path_and_query>')
+def check_url(hostname_port, path_and_query=None):
+    host_unsafe = False   # HOSTNAME is in list of infected sites
+    host_infected = False # HOSTNAME:PORT is in list of affected sites
+    url_infected = False # URL is in list of affected sites
+    
     hostname_port_list = hostname_port.split(":")
+    h_under_test = "http://" + hostname_port_list[0]
+
+    if not validators.url(h_under_test):
+        return "Hostname under test not Valid"
 
     rows=get_url_data(hostname_port_list[0])
+    print(str(rows))
     for url in rows:
         if hostname_port_list[0] in url[0]:
-            url_unsafe = True
+            host_unsafe = True
         if len(hostname_port_list)>1 and hostname_port_list[1] in url[1]:
-            url_infected = True
-        if path_and_query in url[2]:
-            query_infected = True
+            if host_unsafe:
+                host_infected = True
+        if host_unsafe and not len(hostname_port_list)>1:
+            host_infected = True
+        if path_and_query is not None:
+            if path_and_query in url[2] and host_unsafe:
+                url_infected = True
 
-    return json_response(url_unsafe=url_unsafe, url_infected=url_infected, query_infected=query_infected)
+    return json_response(host_unsafe=host_unsafe, host_infected=host_infected, url_infected=url_infected)
 
 def get_url_data(hostname):
     conn = sqlite3.connect('database.db')
@@ -30,20 +42,26 @@ def get_url_data(hostname):
     create_db(conn)
 
     cur = conn.cursor()
-    cur.execute("SELECT * FROM students WHERE url LIKE ?", ('%' + hostname + '%',))
+    cur.execute("SELECT * FROM url")
     rows = cur.fetchall()
     conn.close()
 
     return rows
 
 def create_db(conn):
-    if not conn.execute('CREATE TABLE IF NOT EXISTS students (url TEXT, port TEXT, query TEXT)'):
-        print("Database already present")
-        cur = conn.cursor()
-        cur.execute("INSERT INTO students (url,port,query) VALUES ('www.infected1.com','4000','malwarequery=2')")
-        cur.execute("INSERT INTO students (url,port,query) VALUES ('www.infected1.com','6000', '')")
-        cur.execute("INSERT INTO students (url,port,query) VALUES ('www.infected2.com','4000','malwarequery=1')")
-    print("Table created successfully")
+    cur = conn.cursor()
+    cur.execute("DROP TABLE url")
+    cur.execute(''' SELECT count(*) FROM sqlite_master WHERE type='table' AND name='url' ''')
+    if not cur.fetchone()[0]:
+        cur.execute('CREATE TABLE url (url TEXT, port TEXT, query TEXT)')
+        cur.execute("INSERT INTO url (url,port,query) VALUES ('www.infected1.com','4000','malwarequery=2')")
+        cur.execute("INSERT INTO url (url,port,query) VALUES ('www.infected1.com','6000', '')")
+        cur.execute("INSERT INTO url (url,port,query) VALUES ('www.infected2.com','4000','malwarequery=1')")
+        cur.execute("INSERT INTO url (url,port,query) VALUES ('www.infected2.com','','malwarequery=1')")
+        cur.execute("INSERT INTO url (url,port,query) VALUES ('www.infected3.com','','')")
+        print("Table created successfully")
+    else:
+        print("Table already exists")
 
 if __name__ == '__main__':
     app.run()
--- tests/urltest.py
+++ tests/urltest.py
@@ -2,15 +2,63 @@ import urllib.request
 import pytest
 
 
+def test_invalid_url():
+    """
+    Test that error message is shown when invalid url is given
+    """
+    url = "http://127.0.0.1:5000/urlinfo/1/infectedsite:3000/"
+    r = urllib.request.urlopen(url).read()
+    assert r == b'Hostname under test not Valid'
+
 def test_url_not_infected():
+    """
+    Test that the url is not infected
+    """
     url = "http://127.0.0.1:5000/urlinfo/1/infectedsite.org:3000/abcd"
     r = urllib.request.urlopen(url).read()
-    assert r == b'{"query_infected":false,"status":200,"url_infected":false,"url_unsafe":false}\n'
-    print(urllib.request.urlopen(url).read())
+    assert r == b'{"host_infected":false,"host_unsafe":false,"status":200,"url_infected":false}\n'
 
 def test_url_infected():
+    """
+    Test that url is infected. All parts of url are infected
+    """
+    url = "http://127.0.0.1:5000/urlinfo/1/www.infected1.com:4000/malwarequery=2"
+    r = urllib.request.urlopen(url).read()
+    assert r == b'{"host_infected":true,"host_unsafe":true,"status":200,"url_infected":true}\n'
+
+def test_unsafe_no_port_url():
+    """
+    Test that if port is not specified, but hostname is in list of infected host ids,
+    host_infected is true
+    """
     url = "http://127.0.0.1:5000/urlinfo/1/www.infected1.com/malwarequery=2"
     r = urllib.request.urlopen(url).read()
-    assert r == b'{"query_infected":true,"status":200,"url_infected":false,"url_unsafe":true}\n'
-    print(urllib.request.urlopen(url).read())
+    assert r == b'{"host_infected":true,"host_unsafe":true,"status":200,"url_infected":true}\n'
+
+def test_unsafe_host_port_not_query():
+    """
+    Test host and port are in list of infected but, whole url is not.
+    In this case url_infected is set to false but host_infected is true
+    """
+    url = "http://127.0.0.1:5000/urlinfo/1/www.infected1.com/abcd"
+    r = urllib.request.urlopen(url).read()
+    assert r == b'{"host_infected":true,"host_unsafe":true,"status":200,"url_infected":false}\n'
+
+def test_no_query():
+    """
+    Test if query is not specified.
+    In this case url_infected is set to false but host_infected is true if infected.
+    """
+    url = "http://127.0.0.1:5000/urlinfo/1/www.infected1.com:4000/"
+    r = urllib.request.urlopen(url).read()
+    assert r == b'{"host_infected":true,"host_unsafe":true,"status":200,"url_infected":false}\n'
+
+def test_unsafe_host_safe_port():
+    """
+    Test that host is infected but port is not in list of infected ports.
+    In this case host_unsafe is true, host_infected is false.
+    """
+    url = "http://127.0.0.1:5000/urlinfo/1/www.infected1.com:8000/malwarequery=2"
+    r = urllib.request.urlopen(url).read()
+    assert r == b'{"host_infected":false,"host_unsafe":true,"status":200,"url_infected":true}\n'
 
